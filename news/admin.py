5435acbab632b85cb1fe72c584192454ac25aec8
Taizul Islam <taizulislam@Taizuls-MacBook-Pro.local>
2023-02-09 09:29:42 +0600

    Add category and tag models

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Tag, Category
 
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post, Category, Tag)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
--- news/models.py
+++ news/models.py
@@ -3,12 +3,39 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 50)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        
+
+
+    def __str__(self):
+        return self.name
+    
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     features_image = models.ImageField(upload_to='static/src/images', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self) -> str:
         return self.title
+    
