29e6a766e2aab01f8effc3d427d3481edcf3f35b
Taizul Islam <taizulislam@Taizuls-MacBook-Pro.local>
2023-02-10 23:44:24 +0600

    Add register view and form

--- base/urls.py
+++ base/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from authentication import views as auth_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("register/", auth_view.register, name="register"),
     path('', include('news.urls'))
 ]
