9e2afde3e6060eed4261e49e417342584ddf587b
Taizul Islam <taizulislam@Taizuls-MacBook-Pro.local>
2023-02-10 23:24:49 +0600

    Add authentication app

--- base/settings.py
+++ base/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'news.apps.NewsConfig'
+    'news.apps.NewsConfig',
+    'authentication.apps.AuthenticationConfig'
 ]
 
 MIDDLEWARE = [
