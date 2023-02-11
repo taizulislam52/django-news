d178db7174df1fd451b4d1b6d7dff0d2a30604f5
Taizul Islam <taizulislam@Taizuls-MacBook-Pro.local>
2023-02-11 12:27:16 +0600

    Add email, first last name to user registration form

--- authentication/views.py
+++ authentication/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 from django.contrib import messages
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account successfully created for {username}.')
             return redirect('news-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
