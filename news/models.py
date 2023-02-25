1a94d03e8fb1c2d45a7a64e5736a8a29a6577a6f
Taizul Islam <taizulislam@Taizuls-MacBook-Pro.local>
2023-02-25 15:00:47 +0600

    Add default features image

--- news/models.py
+++ news/models.py
@@ -36,9 +36,9 @@ class Tag(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField(null=True, unique=True)
+    slug = models.SlugField(max_length=100,null=True, unique=True)
     content = models.TextField()
-    features_image = models.ImageField(upload_to='static/src/images', blank=True)
+    features_image = models.ImageField(upload_to='static/src/images', blank=True, default='static/src/images/default.png')
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
@@ -50,6 +50,13 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("news-single", kwargs={"slug": self.slug})
 
+    @property
+    def get_features_image_url(self):
+        if self.features_image and hasattr(self.features_image, 'url'):
+            return self.features_image.url
+        else:
+            return '/static/src/images/default.png'
+    
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
